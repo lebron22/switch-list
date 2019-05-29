251243383dec073752c2c57ae7f80192aaddd45c
Kamil Bronisz <kbronisz8@gmail.com>
2019-05-29 21:02:44 +0200

    Updates

---
+++ precache-manifest.6032611d2fa1b68c488c668402b41040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cecf45e5c240e1e8feee4dc6f82d59",
+    "url": "/switch-list/index.html"
+  },
+  {
+    "revision": "7f3ed63de4907d25290a",
+    "url": "/switch-list/static/css/main.33e9ba22.chunk.css"
+  },
+  {
+    "revision": "87e913273ec8e6e3b27a",
+    "url": "/switch-list/static/js/2.fa9dac84.chunk.js"
+  },
+  {
+    "revision": "7f3ed63de4907d25290a",
+    "url": "/switch-list/static/js/main.5d2f8e39.chunk.js"
+  },
+  {
+    "revision": "a6988d755ff6672cfc3c",
+    "url": "/switch-list/static/js/runtime~main.1f7c6dc0.js"
+  }
+]);
