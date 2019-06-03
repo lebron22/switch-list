3738d6ae0ff77c08ebd095e7638d56ee750161db
Kamil Bronisz <kbronisz8@gmail.com>
2019-06-03 13:21:37 +0200

    Change package.json to deploy

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Switch from '../components/Switch';
-import PanelBody from './PanelBody';
+import React from "react";
+import Switch from "../components/Switch";
+import PanelBody from "./PanelBody";
 
 const Panel = ({ post, handleSwitchChange }) => {
-    const { title, body, id, isOpened } = post
-    return (
-        <section>
-            <header className='panel__header'>
-                <Switch checked={isOpened} handleSwitchChange={() => handleSwitchChange(id)} />
-                <h3 className='panel__heading'>{title}</h3>
-            </header >
-            <PanelBody body={body} isOpened={isOpened} />
-        </section>
-    );
-}
+  const { title, body, id, isOpened } = post;
+  return (
+    <section>
+      <header className="panel__header">
+        <Switch
+          checked={isOpened}
+          handleSwitchChange={() => handleSwitchChange(id)}
+        />
+        <h3 className="panel__heading">{title}</h3>
+      </header>
+      <PanelBody body={body} isOpened={isOpened} />
+    </section>
+  );
+};
 
 export default Panel;
